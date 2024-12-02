fe180ececd06fe7f6e15da0ee9380036bbd84a77
ronazfarahmand <ronazfarahmand1@gmail.com>
2024-12-02 00:12:44 -0500

    2024 - day 1 - part 2

--- 2024/day1.cpp
+++ 2024/day1.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 using namespace std;
 
-int getDistance() {
-    int res = 0;
+vector<vector<int>> getVectors() {
     vector<int> vec1, vec2;
 
     int i = 0;
@@ -17,20 +16,39 @@ int getDistance() {
         i++;
     }
 
+    return {vec1, vec2};
+}
+
+int getDistance(vector<int> vec1, vector<int> vec2) {
+    int res = 0;
+
     sort(vec1.begin(), vec1.end());
     sort(vec2.begin(), vec2.end());
 
     for (int i = 0; i < vec1.size(); i++) {
-        res += abs(vec1.at(i) - vec2.at(i));
+        res += abs(vec1[i] - vec2[i]);
+    }
+
+    return res;
+}
+
+int getSimilarity(vector<int> vec1, vector<int> vec2) {
+    int res = 0;
+
+    for (int i = 0; i < vec1.size(); i++) {
+        res += vec1[i] * count(vec2.begin(), vec2.end(), vec1[i]);
     }
 
     return res;
 }
 
 int main () {
-    int distance = getDistance();
+    vector<vector<int>> vecList = getVectors();
+    int distance = getDistance(vecList[0], vecList[1]);
+    int similarity = getSimilarity(vecList[0], vecList[1]);
 
     cout << "Distance is: " << distance << endl;
+    cout << "Similarity score is: " << similarity << endl;
 
     return 0;
 }
