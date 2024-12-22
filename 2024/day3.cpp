5bd299ccd97a28301c5db86b3477f083c9a45f6a
ronazfarahmand <ronazfarahmand1@gmail.com>
2024-12-22 17:05:53 -0500

    2024 - day 3 - part 2

--- 2024/day3.cpp
+++ 2024/day3.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
-#include <sstream>
-#include <vector>
 using namespace std;
-
-const int MIN_DIST = 1;
-const int MAX_DIST = 3;
-
-void mulScan(string s) {
-}
 
 int main () {
     int mulRes = 0;
+
+    string doStr = "do()";
+    string dontStr = "don't()";
+    bool enabled = true;
 
     string mulStr = "mul(";
     char charBet = ',';
@@ -19,7 +15,19 @@ int main () {
     string s;
     while (cin >> c) {
         s = s + c;
-        if (s != mulStr.substr(0, s.size())) {
+        if (s != mulStr.substr(0, s.size()) &&
+            s != doStr.substr(0, s.size()) && 
+            s != dontStr.substr(0, s.size())) {
+            s = "";
+        }
+
+        if (s == doStr) {
+            enabled = true;
+            s = "";
+        }
+
+        if (s == dontStr) {
+            enabled = false;
             s = "";
         }
 
@@ -30,7 +38,7 @@ int main () {
             char end;
 
             if (cin >> a && cin >> bet && bet == charBet && cin >> b && cin >> end && end == charEnd) {
-                mulRes += a * b;
+                mulRes += a * b * enabled;
             }
 
             s = "";
