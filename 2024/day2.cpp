fd4a27d8aabc2e79ce36edef57c715b33b77590a
ronazfarahmand <ronazfarahmand1@gmail.com>
2024-12-22 16:18:33 -0500

    2024 - day 2 - part 2

--- 2024/day2.cpp
+++ 2024/day2.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 using namespace std;
 
+const int MIN_DIST = 1;
+const int MAX_DIST = 3;
+
 vector<vector<int>> getVector() {
     vector<vector<int>> vec;
 
@@ -21,15 +24,73 @@ vector<vector<int>> getVector() {
     return vec;
 }
 
-bool getValidity(const vector<int> &vec) {
-    int growth = 0, minDist = 1, maxDist = 3;
+bool distanceRemove(vector<int> &vec, int &index) {
+    if (index == vec.size() - 1) {
+        vec.erase(vec.begin() + index);
+        index--;
+
+        return true;
+    }
+
+    int dist = abs(vec[index - 1] - vec[index + 1]);
+    if (dist < MIN_DIST || dist > MAX_DIST) {
+        vec.erase(vec.begin() + index - 1);
+        index--;
+
+        return true;
+    }
+
+    vec.erase(vec.begin() + index);
+    index--;
+
+    return true;
+}
+
+bool growthRemove(vector<int> &vec, int &index, int &growth) {
+    if (index == vec.size() - 1) {
+        return true;
+    }
+
+    int dist = abs(vec[index - 1] - vec[index + 1]);
+
+    if (dist == 0) {
+        vec.erase(vec.begin() + index - 1);
+        index--;
+
+        return true;
+    }
+
+    int relGrowth = (vec[index + 1] - vec[index - 1]) / dist;
+
+    if (growth != relGrowth) {
+        vec.erase(vec.begin() + index - 1);
+        index--;
+
+        return true;
+    }
+
+    vec.erase(vec.begin() + index);
+    index--;
+    
+    return true;
+}
+
+bool getValidity(vector<int> &vec) {
+    int removed = 0, growth = 0;
 
     for (int i = 1; i < vec.size(); i++) {
         int dist = abs(vec[i] - vec[i - 1]);
 
-        if (dist < minDist || dist > maxDist) {
-            return false;
+        if (dist < MIN_DIST || dist > MAX_DIST) {
+            distanceRemove(vec, i);
+
+            dist = abs(vec[i] - vec[i - 1]);
+            if (dist == 0) {
+                return false;
+            }
+            removed++;
         }
+
         int relGrowth = (vec[i] - vec[i - 1]) / dist;
 
         if (growth == 0) {
@@ -37,6 +98,11 @@ bool getValidity(const vector<int> &vec) {
         }
 
         if (growth != relGrowth) {
+            growthRemove(vec, i, growth);
+            removed++;
+        }
+
+        if (removed > 1) {
             return false;
         }
     }
